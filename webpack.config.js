6eb43ed6baeb64297502e715ebe3f50919174b35
Richard Ben <frichardben@yahoo.com.br>
2021-12-18 00:34:41 -0300

    chore: config dev server

--- webpack.config.js
+++ webpack.config.js
@@ -6,45 +6,49 @@ const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin'
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
 module.exports = {
-    mode: isDevelopment ? 'development' : 'production',
-    devtool: isDevelopment ? 'eval-source-map' : 'source-map',
-    entry: path.resolve(__dirname, 'src', 'index.tsx'),
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-      },
-    resolve: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx']
-    },
-    devServer: {
-      static: path.resolve(__dirname, 'public'),
-      hot: true,
-    },
-    plugins: [
-      isDevelopment && new ReactRefreshWebpackPlugin(),
-      new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, 'public', 'index.html')
-      })
-    ],
-    module: {
-        rules: [
-          {
-            test: /\.(j|t)sx$/,
-            exclude: /node_modules/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                plugins: [
-                  isDevelopment && require.resolve('react-refresh/babel')
-                ].filter(Boolean)
-              }
-            }
-          },
-          {
-            test: /\.scss$/,
-            exclude: /node_modules/,
-            use: ['style-loader', 'css-loader', 'sass-loader']
+  mode: isDevelopment ? 'development' : 'production',
+  devtool: isDevelopment ? 'eval-source-map' : 'source-map',
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'public'),
+    hot: true
+  },
+  plugins: [
+    isDevelopment && new ReactRefreshWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)sx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              isDevelopment && require.resolve('react-refresh/babel')
+            ].filter(Boolean)
           }
-        ]
-    }
+        }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif|woff|woff2|eot|ttf|otf)$/i,
+        use: { loader: 'file-loader' }
+      }
+    ]
+  }
 }

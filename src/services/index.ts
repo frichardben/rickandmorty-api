f35e7bf1d67b857471c896657911c8dbcef9406d
Richard Ben <frichardben@yahoo.com.br>
2022-01-18 21:26:12 -0300

    fix:  error axios api

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,5 +1,8 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'https://rickandmortyapi.com/api'
+  baseURL: 'https://rickandmortyapi.com/api',
+  headers: {
+    'Access-Control-Allow-Origin': '*'
+  }
 })

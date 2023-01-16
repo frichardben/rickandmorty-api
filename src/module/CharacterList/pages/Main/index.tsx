3051cba98aec3e53ebe0fbf7cfdd84f75f24077d
Richard Ben <frichardben@yahoo.com.br>
2023-01-16 20:40:18 -0300

    feat: added search character

--- src/module/CharacterList/pages/Main/index.tsx
+++ src/module/CharacterList/pages/Main/index.tsx
@@ -16,13 +16,14 @@ export const Main = () => {
   const [characterList, setCharacterList] = useState<Character[]>([])
   const [error, setError] = useState<string>('')
   const [loading, setLoading] = useState(false)
+  const [search, setSearch] = useState('')
 
 
   const fetchData = () => {
     CharacterService.getAllCharacter().then((res: any) => {
+      setLoading(!loading)
       setCharacterList(res.data.results)
       setLoading(!loading)
-      console.log(res.data.results)
     }).catch((err: Error) => {
       setError("Algo deu errado!")
       console.log(err)
@@ -33,13 +34,22 @@ export const Main = () => {
     fetchData()
   }, [])
 
+  const filteredCharacter = search.length > 0
+    ? characterList.filter(character => character.name.includes(search))
+    : []
+
   return (
     <>
-      <Search />
+      <Search Onsearch={setSearch} search={search}/>
       <main>
         <section>
           <Suspense fallback={renderLoader()}>
-            <CharacterCard character={characterList} error={error}/>
+            {search.length > 0 ? (
+              <CharacterCard character={filteredCharacter} error={error}/>
+            ): (
+              <CharacterCard character={characterList} error={error}/>
+            )}
+
           </Suspense>
         </section>
       </main>
